323b506b739bed9b445de3e16e162011228a24e3
darkHucks <cindybeelly@gmail.com>
2020-05-14 17:37:37 +0100

    all

--- src/components/Humanit.jsx
+++ src/components/Humanit.jsx
@@ -1,6 +1,6 @@
-import React,{ useState } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import '../styles/humanit.css';
 import Frame from './Frame'
 import SpecificFrame from './SpecificFrame'
 
@@ -14,43 +14,45 @@ import hum4 from '../Images/humnitarian/mask1.jpg'
 
 
 const Humanit = () => {
-const [hums, updateHums] = useState([
-                        {id:"efams/Food", img:hum1, title:"Food and Groceries",  story:"The lockdown measures in Affected Regions translate into layoffs and reduced income, making it more of a struggle for many families to put food on the table.We buy stocks,ranging from finished products to large raw food produce from manufacturers with raw stack or finished goods,Large scale farmers.Repack into portable sizes and deliver to appropriate bodies for proper distribution in affected zones. "},
-                        {id:"efams/Mask", img:hum4, title:"Personal Protective Equipment", story:`EFAMS is coordinating with public health authorities to provide personal protective equipment, or PPE, to health care workers in areas affected by the COVID-19 outbreak. The organization’s most urgent need is to procure and replenish additional PPE items for future deployment. Expanding Our coronavirus response efforts through the large-scale provision of PPE  essentials to not just medical personels but also regular citizen of affected communities.`},
-                        {id:"efams/Medical", img:hum3, title:"Medical Supplies", story:`Aiding and purchase of suplies from  medical manufacturers, and is suppling hospitals,  sharing its assessment and inventory availability with public health agencies.
+    const [hums, updateHums] = useState([
+        { id: "efams/Food", img: hum1, title: "Food and Groceries", story: "The lockdown measures in Affected Regions translate into layoffs and reduced income, making it more of a struggle for many families to put food on the table.We buy stocks,ranging from finished products to large raw food produce from manufacturers with raw stack or finished goods,Large scale farmers.Repack into portable sizes and deliver to appropriate bodies for proper distribution in affected zones. " },
+        { id: "efams/Mask", img: hum4, title: "Personal Protective Equipment", story: `EFAMS is coordinating with public health authorities to provide personal protective equipment, or PPE, to health care workers in areas affected by the COVID-19 outbreak. The organization’s most urgent need is to procure and replenish additional PPE items for future deployment. Expanding Our coronavirus response efforts through the large-scale provision of PPE  essentials to not just medical personels but also regular citizen of affected communities.` },
+        {
+            id: "efams/Medical", img: hum3, title: "Medical Supplies", story: `Aiding and purchase of suplies from  medical manufacturers, and is suppling hospitals,  sharing its assessment and inventory availability with public health agencies.
                         Delivering oxygen concentrators to have available for COVID-19 patients who may need oxygen supplementation during their recovery.
                         Oxygen concentrators can free up ICU beds by allowing patients to recover after they’ve been discharged while still receiving breathing assistance.`},
-                        {id:"efams/Education", img:hum2, title:"Research and Education", story:`
+        {
+            id: "efams/Education", img: hum2, title: "Research and Education", story: `
                         Building an ICU medication model and mobilizing private resources to build a stockpile to assist with anticipated spike in ICU patients
                         Boosting support to safety-net facilities to address existing chronic gaps that are likely to grow
                         Collaborating with infectious disease epidemiologists, technology companies and public agencies to use aggregated mobility data to support the COVID-19 response.`}
-                    ])
-const [ Food, updateFood ] = useState( {id:"s_f1", type:"food"} )
-const [ Mask, updateMask ] = useState( {id:"s_f2", type:"mask"} )
-const [ Medical, updateMedical ] = useState( {id:"s_f3", type:"medical"} )
-const [ Education, UpdateEducation ] = useState( {id:"s_f4", type:"research"} )
-    
+    ])
+    const [Food, updateFood] = useState({ id: "s_f1", type: "food" })
+    const [Mask, updateMask] = useState({ id: "s_f2", type: "mask" })
+    const [Medical, updateMedical] = useState({ id: "s_f3", type: "medical" })
+    const [Education, UpdateEducation] = useState({ id: "s_f4", type: "research" })
+
     return (
-        <div style={{backgroundColor: '#EFF0F1',width: '100%',padding: "30px"}}>
-        <BrowserRouter>
-        <h3>Relief Categories</h3>
-            <Route path="/efams" exact >
-                <Frame docs={hums}/>
-            </Route>
-            <Route path="/efams/Food" exact>
-                <SpecificFrame docs={Food}/>
-            </Route>
-            <Route path="/efams/Mask" exact>
-                <SpecificFrame docs={Mask}/>
-            </Route>
-            <Route path="/efams/Medical" exact>
-                <SpecificFrame docs={Medical}/>
-            </Route>
-            <Route path="/efams/Education" exact>
-                <SpecificFrame docs={Education}/>
-            </Route>
-            
-        </BrowserRouter>
+        <div className="init" >
+            <BrowserRouter>
+                <h3>Relief Categories</h3>
+                <Route path="/efams" exact >
+                    <Frame docs={hums} />
+                </Route>
+                <Route path="/efams/Food" exact>
+                    <SpecificFrame docs={Food} />
+                </Route>
+                <Route path="/efams/Mask" exact>
+                    <SpecificFrame docs={Mask} />
+                </Route>
+                <Route path="/efams/Medical" exact>
+                    <SpecificFrame docs={Medical} />
+                </Route>
+                <Route path="/efams/Education" exact>
+                    <SpecificFrame docs={Education} />
+                </Route>
+
+            </BrowserRouter>
         </div>
     );
 }
--- src/components/Message1.jsx
+++ src/components/Message1.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/message1.css'
 
 import { bounceInUp } from 'react-animations';
-import Radium, {StyleRoot} from 'radium';
+import Radium, { StyleRoot } from 'radium';
 
 const Message1 = () => {
 
@@ -18,37 +18,37 @@ const Message1 = () => {
 
     return (
         <StyleRoot>
-        <div style={styles.bounceInUp}>
-            <div className="message">
-                <h2 > Emergency Fund Accredition and Management Scheme(EFAMS)</h2>
-                <h2>A Sub-division of World Health Organization(WHO)</h2>
-                <h3>
-                The outbreak and spread of the coronavirus has already created massive disruption around the world, to both businesses and individuals, as well as huge economic uncertainty going forward. The number of confirmed cases grows everyday and, with no market or geography seemingly immune, the difficulties continue to mount.
-                The World is struggling to survive the pandemic, with communities and economies
-                    affected barely able to stand.
+            <div  style={styles.bounceInUp}>
+                <div className="message">
+                    <h2 > Emergency Fund Accredition and Management Scheme(EFAMS)</h2>
+                    <h2>A Sub-division of World Health Organization(WHO)</h2>
+                    <h3>
+                        The outbreak and spread of the coronavirus has already created massive disruption around the world, to both businesses and individuals, as well as huge economic uncertainty going forward. The number of confirmed cases grows everyday and, with no market or geography seemingly immune, the difficulties continue to mount.
+                        The World is struggling to survive the pandemic, with communities and economies
+                        affected barely able to stand.
                 </h3>
-                
-                <p>
-                    In Response to the Increasing Need For Extensive Relief Packages and Health Care Supplies
+
+                    <p>
+                        In Response to the Increasing Need For Extensive Relief Packages and Health Care Supplies
             </p>
-                <p>
-                    We Are Seeking individual and Organization supports, To Extend Our Reach To The Core Locations In Need.
+                    <p>
+                        We Are Seeking individual and Organization supports, To Extend Our Reach To The Core Locations In Need.
             </p>
-                <p>
-                    The Strategic Preparedness and Response Plan outlines a funding need of at least US$675 million for critical response efforts in countries most in need of help through April 2020. As this outbreak evolves, funding needs are likely to increase.
+                    <p>
+                        The Strategic Preparedness and Response Plan outlines a funding need of at least US$675 million for critical response efforts in countries most in need of help through April 2020. As this outbreak evolves, funding needs are likely to increase.
             </p>
-                <p>
-                    As COVID-19 continues to evolve, It reinforces the need for us to act together to keep not only our families, employees and communities healthy, but also our businesses and the economy.
+                    <p>
+                        As COVID-19 continues to evolve, It reinforces the need for us to act together to keep not only our families, employees and communities healthy, but also our businesses and the economy.
             </p>
-                <p>
-                    We Added Special Forumn For Organization And Individuals With Substantial Amount To Lease to The Cause, Pending the passing of the Period.  
+                    <p>
+                        We Added Special Forumn For Organization And Individuals With Substantial Amount To Lease to The Cause, Pending the passing of the Period.
                 </p>
-                <p>
-                Your Donation Is Greatly Appreciated In The Global Effort Supporting The  Ability Of All Countries To Respond to COVID-19, Especially Where The Needs Are Greatest And In Countries With Less Access To Global Markets And Lower Resources
+                    <p>
+                        Your Donation Is Greatly Appreciated In The Global Effort Supporting The  Ability Of All Countries To Respond to COVID-19, Especially Where The Needs Are Greatest And In Countries With Less Access To Global Markets And Lower Resources
                 </p>
+                </div>
+
             </div>
-            
-        </div>
         </StyleRoot>
     );
 }
